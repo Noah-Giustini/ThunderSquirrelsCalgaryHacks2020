9695cadebd498c679dd54fd93c28845dd32f85c7
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2020-02-15 13:12:26 -0700

    init

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+<style type="text/css">
+	body{
+		text-align: center;
+		padding-bottom: 100px;
+		padding-top: 50px;
+	}
+	#footer {
+	   position:fixed;
+	   bottom:0;
+	   width:100%;
+	   height:60px;
+	   background:#6cf;
+	}
+	header{
+		position: absolute;
+		top:0;
+		width: 100%;
+		height: 60px;
+		background: #6af;
+		padding-bottom: 20px;
+	}
+</style>
+
+<header>
+	<h1> Hospital Database Management System</h1>
+</header>
+
+<body>
+
+<img src="hospital-logo.jpg" alt="logo-main" height = "400" width = "400" align="center">
+<br><br><br>
+<strong><font size="+5">Login</font></strong>
+<br><br><br>
+
+
+<table style="vertical-align: bottom; margin-left: auto; margin-right: auto;">
+	<tr>
+		<th><img src="admin-login1.jpg" alt="admin-login1" height="200" width="200"></th>
+		<th><img src="doctor-login.jpg" alt="doctor-login" height="200" width="200"></th>
+		<th><img src="nurse-login.jpg" alt="nurse-login" height="200" width="200"></th>
+		<th><img src="patient-login.jpg" alt="patient-login1" height="200" width="200"></th>
+	</tr><br>
+
+	<tr>
+		<th><a href="admin/login.php"><button>Admin Login</button></a></th>
+		<th><a href="doctor/login.php"><button>Doctor Login</button></a></th>
+		<th><a href="nurse/login.php"><button>Nurse Login</button></a></th>
+		<th><a href="patient/login.php"><button>Patient Login</button></a></th>
+	</tr>
+
+</table>
+<br><br>
+
+<div id="footer">
+	Hospital Database Management System<br>
+	Created By Nathan Darby, Noah Giustini, and Erslan Salman<br><br><br>
+</div>
+</body>
+</html>
